7d67696f3a035ce03e0c4fcc0f96af6828debba4
Lindemberg da Silva Justino <lindembergparaiba@gmail.com>
2026-09-03 11:56:39 -0300

    feat: Adicionado a tela de itens dos pedidos na Area Admin

--- Areas/Admin/Controllers/AdminPedidosController.cs
+++ Areas/Admin/Controllers/AdminPedidosController.cs
@@ -1,5 +1,6 @@
-﻿using LanchesMac.Context;
+using LanchesMac.Context;
 using LanchesMac.Models;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -40,8 +41,8 @@ namespace LanchesMac.Areas.Admin.Controllers
         }
 
 
-            // GET: Admin/AdminPedidos/Details/5
-         public async Task<IActionResult> Details(int? id)
+        // GET: Admin/AdminPedidos/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
@@ -164,6 +165,28 @@ namespace LanchesMac.Areas.Admin.Controllers
         private bool PedidoExists(int id)
         {
             return _context.Pedidos.Any(e => e.PedidoId == id);
+        }
+
+        public IActionResult PedidoLanches(int? id)
+        {
+            var pedido = _context.Pedidos
+                         .Include(pd => pd.PedidoItens)
+                         .ThenInclude(l => l.Lanche)
+                         .FirstOrDefault(p => p.PedidoId == id);
+
+            if (pedido == null)
+            {
+                Response.StatusCode = 404;
+                return View("PedidoNotFound", id.Value);
+            }
+
+            PedidoLancheViewModel pedidoLanches = new()
+            {
+                Pedido = pedido,
+                PedidoDetalhes = pedido.PedidoItens
+            };
+
+            return View(pedidoLanches);
         }
     }
 }
